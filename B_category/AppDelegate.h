949bb2fe19e59f05d5d355fd1150997fc33c3796
linxiangyu2 <linxiangyu2@yy.com>
2020-11-15 09:10:14 +0800

    Initial Commit

---
+++ B_category/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  B_category
+//
+//  Created by Sean on 2020/11/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ B_category/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  B_category
+//
+//  Created by Sean on 2020/11/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
